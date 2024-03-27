69e21594a7b9d7ebd12d077c81c75f2177d52e6e
Jon A <jonangioi03@gmail.com>
2024-03-27 11:37:42 -0400

    Basic weapons made- basic, rifle, machine gun

---
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+/// <summary>
+///  Weapon class is a base class for all weapons in the game.
+/// </summary>
+public class Weapon
+{
+    public bool IsUnlocked;
+    public string Name;
+    public float FireRate;
+    public float Damage;
+    public GameObject BulletPrefab;
+    public RuleTile.TilingRuleOutput.Transform ShootingPoint;
+
+    private float _nextFireTime = 0f;
+    
+    /// <summary>
+    /// Initialize the weapon with the given parameters.
+    /// </summary>
+    /// <param name="isUnlocked">Is the weapon unlocked?</param>
+    /// <param name="name">Name of the weapon.</param>
+    /// <param name="fireRate">FireRate of the weapon.</param>
+    /// <param name="damage">Damage of the weapon's bullets.</param>
+    /// <returns></returns>
+    public void Initialize(bool isUnlocked, string name, float fireRate, float damage)
+    {
+        this.IsUnlocked = isUnlocked;
+        this.Name = name;
+        this.FireRate = fireRate;
+        this.Damage = damage;
+    }
+
+    /// <summary>
+    /// Checks if the weapon can shoot depending on the fire rate.
+    /// </summary>
+    public void TryShoot()
+    {
+        if (Time.time > nextFireTime)
+        {
+            Shoot();
+            _nextFireTime = Time.time + 1f / fireRate;
+        }
+    }
+    
+    /// <summary>
+    /// Shoots the weapon by instantiating a bullet prefab.
+    /// </summary>
+    protected virtual void Shoot()
+    {
+        Instantiate(BulletPrefab, ShootingPoint.position, Quaternion.identity);
+        // You can add more logic here for the shooting behavior
+    }
+}
---
+++ Assets/Scripts/Weapons/WeaponManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// WeaponManager class is responsible for managing the weapons in the game.
+/// The player can switch between weapons using the Q key. (To be changed to a screen button).
+/// </summary>
+public class WeaponManager : MonoBehaviour
+{
+    public List<Weapon> weapons;
+    private int currentWeaponIndex;
+
+    /// <summary>
+    /// Initialize the weapon manager with a list of weapons.
+    /// </summary>
+    /// <remarks>
+    /// The basic weapon is always unlocked and is the first weapon in the list.
+    /// The rifle and machine gun are locked by default, and must be unlocked by the player.
+    /// Note: This technique for initializing weapons is being used to make it easier to apply upgrades to the weapons.
+    /// </remarks>
+    void Start()
+    {
+        Weapon basic = new Weapon();
+        basic.Initialize(true,"Basic",2, 10);
+        weapons.Add(basic);
+
+        Weapon rifle = new Weapon();
+        rifle.Initialize(false,"Rifle",2, 20);
+        weapons.Add(rifle);
+
+        Weapon machineGun = new Weapon();
+        machineGun.Initialize(false,"MachineGun",10,2)
+        weapons.Add(machineGun);
+
+        currentWeaponIndex = 0;
+        GetComponent<CharacterShooting>().ChangeWeapon(weapons[currentWeaponIndex]);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q)) // Assuming Q switches weapons
+        {
+            currentWeaponIndex = (currentWeaponIndex + 1) % weapons.Count;
+            GetComponent<CharacterShooting>().ChangeWeapon(weapons[currentWeaponIndex]);
+        }
+    }
+}
+
